8d5697ef424cb764dda8c9699b652979ab2e878f
John Doe <john.doe@example.com>
2026-01-15 16:53:11 -0500

    change log returns for simple returns

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def download_stock_data(tickers, start_date, end_date):
 
 
 #Helper functions for calculations
-def calculate_log_returns(prices):
-    return np.log(prices / prices.shift(1)).dropna()
+def calculate_returns(prices):
+    return ((prices / prices.shift(1)) - 1).dropna()
 
-def calculate_annualized_return(log_returns, periods_per_year=252):
-    return (1 + log_returns).prod() ** (periods_per_year / len(log_returns)) - 1
+def calculate_annualized_return(returns, periods_per_year=252):
+    return (1 + returns).prod() ** (periods_per_year / len(returns)) - 1
 
-def calculate_mu_sigma(log_returns, annualized=True, periods_per_year=252):
-    mu = log_returns.mean()
-    sigma = log_returns.std()
+def calculate_mu_sigma(returns, annualized=True, periods_per_year=252):
+    mu = returns.mean()
+    sigma = returns.std()
     if annualized:
         mu = mu * periods_per_year
         sigma = sigma * np.sqrt(periods_per_year)
@@ -28,19 +28,17 @@ def calculate_mu_sigma(log_returns, annualized=True, periods_per_year=252):
 
 
 
-
 if __name__ == "__main__":
     # data = yf.download("AAPL", start="2020-01-01", end="2021-01-01")
     # print(data.head())
-    stocks = download_stock_data(("AAPL", "MSFT"), '2020-01-01', '2021-01-01')
+    stocks = download_stock_data(("AAPL", "MSFT"), '2023-01-01', '2024-01-01')
 
     # print(aapl.head())
 
-    log_returns = calculate_log_returns(stocks)
-    print("expected log returns: ", log_returns.head())
+    returns = calculate_returns(stocks)
+    print("expected returns: ", returns.head())
 
-    expected_log_returns = calculate_annualized_return(log_returns)
-    print("expected log returns: ", expected_log_returns)
-
-    mu, sigma = calculate_mu_sigma(log_returns)
+    expected_returns = calculate_annualized_return(returns)
+    print("expected returns: ", expected_returns)
+    mu, sigma = calculate_mu_sigma(returns)
     print("mu", mu, "sigma", sigma)
